1600400457dda00c90e4a3f21586e9f8c7fdd810
koteyevich <koteyevich@ukr.net>
2025-10-03 23:44:42 +0200

    new /start functions interface

---
+++ xorWallet/BotFunctionality/Interfaces/IStartFunction.cs
@@ -0,0 +1,10 @@
+using Telegram.Bot.Types;
+
+namespace xorWallet.BotFunctionality.Interfaces;
+
+public interface IStartFunction
+{
+    string Name { get; }
+
+    Task ExecuteAsync(Message message, string[] arguments);
+}

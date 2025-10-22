0ad52875876dedaa7220d5a815c2636ba186e860
koteyevich <koteyevich@ukr.net>
2025-10-22 23:24:59 +0200

    rename QR callback

--- xorWallet/BotFunctionality/Callbacks/QR.cs
+++ xorWallet/BotFunctionality/Callbacks/QR.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using QRCoder;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Jpeg;
@@ -5,27 +6,33 @@ using SixLabors.ImageSharp.Processing;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using xorWallet.BotFunctionality.Interfaces;
+using xorWallet.Helpers;
 using xorWallet.Settings;
 
 namespace xorWallet.BotFunctionality.Callbacks;
 
-public class QRCallback : ICallback
+public class QR : ICallback
 {
     public string Name => "qr";
 
     private readonly ITelegramBotClient _bot;
     private readonly IBotInfo _info;
+    private readonly ILogger<QR> _logger;
 
-    public QRCallback(ITelegramBotClient bot, IBotInfo info)
+    public QR(ITelegramBotClient bot, IBotInfo info, ILogger<QR> logger)
     {
         _bot = bot;
         _info = info;
+        _logger = logger;
     }
 
     public async Task ExecuteAsync(CallbackQuery callbackQuery)
     {
-        var data = string.Join('_',
-            callbackQuery.Data!.Split('_', StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray());
+        var data = string.Join(
+            '_',
+            Parser.ParseArguments(callbackQuery.Data!, '_').Skip(1).ToArray()
+        ); // qr_check_kotEy3v1ch... => // check_kotEy3v1ch...
+
         var url = new PayloadGenerator.Url($"https://t.me/{_info.Me.Username}?start={data}");
         var qrGenerator = new QRCodeGenerator();
 
